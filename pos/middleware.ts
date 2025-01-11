815865db8375d64d078950562b991c73e7f879de
p.kulkarni <p.kulkarni@axelor.com>
2025-01-11 23:24:31 +0530

    feat: enhance middleware to protect routes and update matcher configuration

--- pos/middleware.ts
+++ pos/middleware.ts
@@ -1,25 +1,15 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
 // Protecting routes with next-auth
 // https://next-auth.js.org/configuration/nextjs#middleware
 // https://nextjs.org/docs/app/building-your-application/routing/middleware
-// import { NextResponse } from 'next/server';
-// import { auth } from './auth';
+import { NextResponse } from 'next/server';
+import { auth } from './auth';
 
 
-// export default auth((req) => {
-//   if (!req.auth) {
-//     const url = req.url.replace(req.nextUrl.pathname, '/');
-//     return NextResponse.redirect(url)
-//   }
-// });
+export default auth((req) => {
+  if (!req.auth) {
+    const url = req.url.replace(req.nextUrl.pathname, '/');
+    return NextResponse.redirect(url)
+  }
+});
 
-export default function middleware(req: NextRequest) {
-  console.log('Request received:', req.url);
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: '/',
-};
+export const config = { matcher: ['/dash/:path*'] };
