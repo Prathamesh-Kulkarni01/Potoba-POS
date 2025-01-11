5a53df02092e636e64fa8dd9565ee184e10f36e6
p.kulkarni <p.kulkarni@axelor.com>
2025-01-11 16:26:05 +0530

    chore: comment out middleware code for route protection

--- pos/middleware.ts
+++ pos/middleware.ts
@@ -1,15 +1,15 @@
 // Protecting routes with next-auth
 // https://next-auth.js.org/configuration/nextjs#middleware
 // https://nextjs.org/docs/app/building-your-application/routing/middleware
-import { NextResponse } from 'next/server';
-import { auth } from './auth';
+// import { NextResponse } from 'next/server';
+// import { auth } from './auth';
 
 
-export default auth((req) => {
-  if (!req.auth) {
-    const url = req.url.replace(req.nextUrl.pathname, '/');
-    return NextResponse.redirect(url)
-  }
-});
+// export default auth((req) => {
+//   if (!req.auth) {
+//     const url = req.url.replace(req.nextUrl.pathname, '/');
+//     return NextResponse.redirect(url)
+//   }
+// });
 
-export const config = { matcher: ['/dash/:path*'] };
+// export const config = { matcher: ['/dash/:path*'] };
